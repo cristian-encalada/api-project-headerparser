a6565aec4533d3164a0029edca2e1122e18e37df
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-10 19:54:24 -0300

    Try with req.headers['x-forwarded-for']

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/api/hello', function (req, res) {
 // whoami endpoint
 app.get('/api/whoami', function (req, res) {
   res.json({
-    ipadress : req.socket.remoteAddress,
+    ipadress : req.headers['x-forwarded-for'] || req.socket.remoteAddress,
     language: req.headers['accept-language'],
     software: req.headers['user-agent']
   });
